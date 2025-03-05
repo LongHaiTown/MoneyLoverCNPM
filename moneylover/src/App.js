92f57198154284875306f9cc86e7bf45dace6543
Quoc Thaii <qthai0927@gmail.com>
2025-03-05 23:34:49 +0700

    Initial commit

---
+++ moneylover/src/App.js
@@ -0,0 +1,58 @@
+import React, { useState } from "react";
+import { NavbuttonStyle } from "./GlobalStyle";
+function Navbar()
+{
+  const [isOpen, setIsOpen] = useState(false);
+
+  return(
+    <nav 
+    style={{
+        display: "flex", 
+        justifyContent: "space-between", 
+        padding: "10px 20px", 
+        background: "#f8f8f8", 
+        borderBottom: "2px solid #ddd" }} >
+      <div style={{ position: "relative" }}>
+        <button
+          onClick={() => setIsOpen(!isOpen)}
+          style={{ fontSize: "24px", fontWeight: "bold", cursor: "pointer" }}
+        >
+          ☰
+        </button>
+        {isOpen && (
+          <ul
+            style={{
+              position: "absolute",
+              top: "40px",
+              left: "0",
+              background: "white",
+              boxShadow: "0px 4px 6px rgba(0, 0, 0, 0.1)",
+              borderRadius: "5px",
+              listStyle: "none",
+              padding: "10px",
+              margin: "0",
+              minWidth: "120px",
+            }}
+          >
+            <li >Mục 1</li>
+            <li >Mục 2</li>
+            <li >Mục 3</li>
+          </ul>
+        )}
+      </div>
+      <div>
+          <button style={NavbuttonStyle}>Giao dịch</button>
+          <button style={NavbuttonStyle}>Thống kê</button>
+          <button style={NavbuttonStyle}>Ngân sách</button>
+          <button style={{backgroundColor:"#728156", color:"white", margin:"10px", padding:"10px 20px", border: "none", borderRadius:"5px", cursor:"pointer" }}>Long Hai Town</button>
+      </div>
+    </nav>
+  );
+}
+
+function App() {
+  return (
+    <Navbar />);
+}
+
+export default App;
---
+++ moneylover/src/GlobalStyle.js
@@ -0,0 +1,25 @@
+import { createGlobalStyle } from "styled-components";
+
+const GlobalStyles = createGlobalStyle`
+  * {
+    margin: 0;
+    padding: 0;
+    box-sizing: border-box;
+    font-family: Arial, sans-serif;
+  }
+
+  body {
+    background-color: #f5f5f5;
+  }
+`;
+export const NavbuttonStyle ={
+  padding: "10px 20px",
+  backgroundColor: "#007bff",
+  color: "white",
+  border: "none",
+  borderRadius: "5px",
+  cursor: "pointer",
+   margin:"10px"
+};
+
+export default GlobalStyles;
