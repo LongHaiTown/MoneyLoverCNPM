15c899ea0016aefffc9815c0e6b91902f8f87f09
Quoc Thaii <qthai0927@gmail.com>
2025-03-06 21:38:48 +0700

    10h30 lam tiep

--- moneylover/src/App.js
+++ moneylover/src/App.js
@@ -52,15 +52,58 @@ function Navbar()
     </nav>
   );
 }
-function Information()
-{
-  <h1 
-  onClick={() => console.log("Clicked Tất cả giao dịch!")} 
-  style={{ cursor: "pointer", color: "#007bff" }}
->
-  Tất cả giao dịch
-</h1>
-}
+const Information = () => {
+  return (
+
+    <div style={{display:"flex", flexDirection:"row", justifyContent:"space-between"}}>
+      <h1
+      onClick={() => console.log("Clicked Tất cả giao dịch!")}
+      style={{cursor: "pointer", color: "red", paddingLeft:"10px" }}
+    >
+      Tất cả giao dịch
+    </h1>
+    <div
+      style={{
+        width: "200px",
+        paddingRight: "10px",
+        marginRight:"10px",
+        border: "1px solid #ddd",
+        borderRadius: "10px",
+        textAlign: "center",
+        boxShadow: "0px 2px 5px rgba(0,0,0,0.1)",
+        backgroundColor: "white",
+      }}
+    >
+      <div
+        style={{
+          display:"inline-block",
+          backgroundColor: "#ddd",
+          padding: "5px",
+          borderRadius: "5px",
+          fontWeight: "bold",
+        }}
+      >
+        Ví tiền mặt
+      </div>
+      <div style={{ marginTop: "10px" }}>
+        <img
+          src="https://i.pravatar.cc/40" // Ảnh avatar giả
+          alt="Avatar"
+          style={{
+            width: "40px",
+            height: "40px",
+            borderRadius: "50%",
+            display: "block",
+            margin: "auto",
+          }}
+        />
+        <p style={{ margin: "5px 0", fontWeight: "bold" }}>Hiện có</p>
+        <p style={{ fontSize: "12px", color: "gray" }}>(500,000 VND)</p>
+      </div>
+    </div>
+    </div>
+  );
+};
 
 
 
